85b02d73d32ecc6c3cb0c483797cc172cd82c656
Julia Altenbuchner <julia.altenbuchner.11@ucl.ac.uk>
2018-05-02 15:06:06 +0100

    Get projectList from DB is no connection available

--- app/src/main/java/uk/ac/excites/ucl/sapelliviewer/db/ProjectInfoDao.java
+++ app/src/main/java/uk/ac/excites/ucl/sapelliviewer/db/ProjectInfoDao.java
@@ -3,18 +3,21 @@ package uk.ac.excites.ucl.sapelliviewer.db;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 import android.arch.persistence.room.Update;
 
 import java.util.List;
 
+import io.reactivex.Observable;
+import io.reactivex.Single;
 import uk.ac.excites.ucl.sapelliviewer.datamodel.ProjectInfo;
 
 @Dao
 public interface ProjectInfoDao {
 
-    @Insert
-    void insertProjectInfo(ProjectInfo projectInfo);
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertProjectInfo(List<ProjectInfo> projectInfos);
 
     @Update
     void updateProjectInfo(ProjectInfo projectInfo);
@@ -26,5 +29,8 @@ public interface ProjectInfoDao {
     List<ProjectInfo> getProjectInfo(int id);
 
     @Query("SELECT * FROM ProjectInfo")
-    ProjectInfo getProjectInfos();
+    Single<List<ProjectInfo>> getProjectInfos();
+
+    @Query("DELETE FROM ProjectInfo")
+    public void clearProjectInfos();
 }
