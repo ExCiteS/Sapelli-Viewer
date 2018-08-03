c8c1a4c72452217c7e6f5a54c9d50c8dc18f46a9
Julia Altenbuchner <julia.altenbuchner.11@ucl.ac.uk>
2018-08-03 11:46:02 +0100

    FieldContoller and ValueController implemented

--- app/src/main/java/uk/ac/excites/ucl/sapelliviewer/ui/FieldController.java
+++ app/src/main/java/uk/ac/excites/ucl/sapelliviewer/ui/FieldController.java
@@ -1,47 +1,80 @@
 package uk.ac.excites.ucl.sapelliviewer.ui;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.ToggleButton;
 
-import io.reactivex.Single;
+import java.util.List;
+
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableSingleObserver;
 import io.reactivex.schedulers.Schedulers;
+import uk.ac.excites.ucl.sapelliviewer.R;
 import uk.ac.excites.ucl.sapelliviewer.datamodel.Field;
 import uk.ac.excites.ucl.sapelliviewer.datamodel.LookUpValue;
 import uk.ac.excites.ucl.sapelliviewer.service.DatabaseClient;
 
-public class FieldController extends DatabaseClient {
+class FieldController extends DatabaseClient {
 
     private Context context;
+    private ValueController valueController;
     private ValueAdapter valueAdapter;
+    private CompositeDisposable disposibles;
+    private RecyclerView fieldRecyclerView;
 
-    public FieldController(Context context, ValueAdapter valueAdapter) {
+    FieldController(Context context, RecyclerView fieldRecyclerView, ValueController valueController, int projectId, CompositeDisposable disposibles) {
         super(context);
         this.context = context;
-        this.valueAdapter = valueAdapter;
-
+        this.valueController = valueController;
+        this.valueAdapter = valueController.getAdapter();
+        this.disposibles = disposibles;
+        this.fieldRecyclerView = fieldRecyclerView;
+        setAdapter(projectId);
     }
 
-    public Single<FieldAdapter> getAdapter(int projectId) {
-        return getFields(projectId)
-                .subscribeOn(Schedulers.io())
-                .toObservable().flatMapIterable(fields -> fields)
-                .filter(field -> !field.getKey().equals("DeviceId") && !field.getKey().equals("StartTime") && !field.getKey().equals("EndTime"))
-                .toList()
-                .map(fields -> new FieldAdapter(context, fields, new FieldAdapter.FieldCheckedChangeListener() {
+
+    private void setAdapter(int projectId) {
+        disposibles.add(
+                getFields(projectId)
+                        .subscribeOn(Schedulers.io())
+                        .toObservable().flatMapIterable(fields -> fields)
+                        .filter(field -> !field.getKey().equals("DeviceId") && !field.getKey().equals("StartTime") && !field.getKey().equals("EndTime"))
+                        .toList()
+                        .subscribeWith(new DisposableSingleObserver<List<Field>>() {
+
                             @Override
-                            public void checkedChanged(ToggleButton buttonView, boolean isChecked, Field field) {
-                                for (LookUpValue lookUpValue : valueAdapter.getAllLookUpValues()) {
-                                    if (lookUpValue.getFieldId() == field.getId())
-                                        lookUpValue.setVisible(isChecked);
-                                }
+                            public void onSuccess(List<Field> fields) {
+                                FieldAdapter fieldAdapter = new FieldAdapter(context, fields, new FieldAdapter.FieldCheckedChangeListener() {
+                                    @SuppressLint("CheckResult")
+                                    @Override
+                                    public void checkedChanged(ToggleButton buttonView, boolean isChecked, Field field) {
+                                        for (LookUpValue lookUpValue : valueAdapter.getAllLookUpValues()) {
+                                            if (lookUpValue.getFieldId() == field.getId())
+                                                lookUpValue.setVisible(isChecked);
+                                        }
+                                        if (isChecked) {
+                                            buttonView.setBackgroundColor(ContextCompat.getColor(context, R.color.colorPrimary));
+                                        } else {
+                                            buttonView.setBackgroundColor(Color.WHITE);
+                                        }
+                                        valueAdapter.notifyDataSetChanged();
+                                        loadMarkers(valueAdapter.getVisibleAndActiveLookupValues()).subscribe(valueController::updateMarkers);
+                                    }
+                                });
+                                fieldRecyclerView.setAdapter(fieldAdapter);
                             }
-                        }
-                        )
-                )
-                .observeOn(AndroidSchedulers.mainThread());
 
-
+                            @Override
+                            public void onError(Throwable e) {
+                                Log.e("getFields", e.getMessage());
+                            }
+                        }));
     }
+
 
 }
--- app/src/main/java/uk/ac/excites/ucl/sapelliviewer/ui/ValueController.java
+++ app/src/main/java/uk/ac/excites/ucl/sapelliviewer/ui/ValueController.java
@@ -1,28 +1,70 @@
 package uk.ac.excites.ucl.sapelliviewer.ui;
 
-import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
-import io.reactivex.Single;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
+import java.util.List;
+
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableSingleObserver;
+import uk.ac.excites.ucl.sapelliviewer.activities.OfflineMapsActivity;
+import uk.ac.excites.ucl.sapelliviewer.datamodel.Contribution;
+import uk.ac.excites.ucl.sapelliviewer.datamodel.LookUpValue;
 import uk.ac.excites.ucl.sapelliviewer.service.DatabaseClient;
 
 public class ValueController extends DatabaseClient {
 
-    private Context context;
+    private OfflineMapsActivity mapsActivity;
+    private ValueAdapter valueAdapter;
+    private RecyclerView valueRecyclerView;
+    private CompositeDisposable disposables;
+    private RecyclerView fieldRecyclerView;
 
-    public ValueController(Context context) {
-        super(context);
-        this.context = context;
+
+    public ValueController(OfflineMapsActivity mapActivity, RecyclerView valueRecyclerView, CompositeDisposable disposables) {
+        super(mapActivity.getApplicationContext());
+        this.mapsActivity = mapActivity;
+        this.valueRecyclerView = valueRecyclerView;
+        valueRecyclerView.setLayoutManager(new LinearLayoutManager(mapActivity.getApplicationContext(), LinearLayoutManager.HORIZONTAL, false));
+        this.disposables = disposables;
+        getValueAdapter();
     }
 
+    private void getValueAdapter() {
+        disposables.add(
+                getLookUpValues(mapsActivity.getProjectId())
+                        .subscribeWith(new DisposableSingleObserver<List<LookUpValue>>() {
+                            @Override
+                            public void onSuccess(List<LookUpValue> lookUpValues) {
+                                valueAdapter = new ValueAdapter(mapsActivity.getApplicationContext(), lookUpValues, (v, value) -> {
+                                    value.setActive(!value.isActive());
+                                    valueAdapter.notifyDataSetChanged();
+                                    loadMarkers(valueAdapter.getVisibleAndActiveLookupValues()).subscribe(contributions -> updateMarkers(contributions));
+                                });
+                                valueRecyclerView.setAdapter(valueAdapter);
+                                if (fieldRecyclerView != null)
+                                    new FieldController(mapsActivity.getApplicationContext(), fieldRecyclerView, ValueController.this, mapsActivity.getProjectId(), disposables);
 
-//    public Single<ValueAdapter> getValueAdapter(int projectId) {
-//        return getLookUpValues(projectId)
-//                .subscribeOn(Schedulers.io())
-//                .map(lookUpValues -> new ValueAdapter(context,lookUpValues))
-//                .observeOn(AndroidSchedulers.mainThread());
-//    }
+                            }
 
+                            @Override
+                            public void onError(Throwable e) {
+                                Log.e("getLookupvalues", e.getMessage());
+                            }
+                        }));
+    }
 
+    public void addFieldController(RecyclerView recyclerView) {
+        this.fieldRecyclerView = recyclerView;
+        fieldRecyclerView.setLayoutManager(new LinearLayoutManager(mapsActivity.getApplicationContext(), LinearLayoutManager.HORIZONTAL, false));
+    }
+
+    public ValueAdapter getAdapter() {
+        return valueAdapter;
+    }
+
+    public void updateMarkers(List<Contribution> contributions) {
+        mapsActivity.updateMarkers(contributions);
+    }
 }
