e228f56097fca57e945a10930aeebe41ef15ee72
Julia Altenbuchner <julia.altenbuchner.11@ucl.ac.uk>
2018-08-06 11:12:13 +0200

    BaseMap activity removed

---
+++ app/src/main/java/uk/ac/excites/ucl/sapelliviewer/ui/DetailsFragement.java
@@ -0,0 +1,18 @@
+package uk.ac.excites.ucl.sapelliviewer.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import uk.ac.excites.ucl.sapelliviewer.R;
+
+public class DetailsFragement extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_details, container, false);
+    }
+}
