8fb8b8d16b626ec596d518570e80083fdc733dfb
Gulbala Salamov <g.salamov@gmail.com>
2020-01-06 22:17:07 +0300

    Network tracker Chucker implemented

--- app/src/main/java/uk/ac/excites/ucl/sapelliviewer/service/RetrofitBuilder.java
+++ app/src/main/java/uk/ac/excites/ucl/sapelliviewer/service/RetrofitBuilder.java
@@ -1,10 +1,13 @@
 package uk.ac.excites.ucl.sapelliviewer.service;
 
+import android.content.Context;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 
 import java.io.IOException;
+import java.lang.reflect.Constructor;
 
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
@@ -12,10 +15,10 @@ import okhttp3.Request;
 import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import uk.ac.excites.ucl.sapelliviewer.BuildConfig;
 import uk.ac.excites.ucl.sapelliviewer.activities.LoginActivity;
 import uk.ac.excites.ucl.sapelliviewer.datamodel.Contribution;
 import uk.ac.excites.ucl.sapelliviewer.datamodel.Geometry;
-import uk.ac.excites.ucl.sapelliviewer.datamodel.Login;
 import uk.ac.excites.ucl.sapelliviewer.utils.TokenManager;
 
 /**
@@ -57,23 +60,37 @@ public class RetrofitBuilder {
 
     public static <T> T createServiceWithAuth(Class<T> service, final TokenManager tokenManager) {
 
-        OkHttpClient newClient = client.newBuilder().addInterceptor(new Interceptor() {
-            @Override
-            public Response intercept(Chain chain) throws IOException {
-                Request request = chain.request();
-                Request.Builder builder = request.newBuilder();
-                if (tokenManager.getToken().getAccess_token() != null) {
-                    builder.addHeader("Authorization", "Bearer " + tokenManager.getToken().getAccess_token());
-                }
+        OkHttpClient.Builder builder = client.newBuilder()
+                .addInterceptor(new Interceptor() {
+                    @Override
+                    public Response intercept(Chain chain) throws IOException {
+                        Request request = chain.request();
+                        Request.Builder builder = request.newBuilder();
+                        if (tokenManager.getToken().getAccess_token() != null) {
+                            builder.addHeader("Authorization", "Bearer " + tokenManager.getToken().getAccess_token());
+                        }
 
-                request = builder.build();
-                return chain.proceed(request);
+                        request = builder.build();
+                        return chain.proceed(request);
+                    }
+                }).authenticator(RefreshAuthenticator.getInstance(tokenManager))
+                .addInterceptor(new ConnectivityInterceptor(LoginActivity.get().getApplicationContext()));
+
+        if (BuildConfig.DEBUG) {
+            Class clazz;
+            Interceptor chuckerInterceptor;
+            try {
+                clazz = Class.forName("com.chuckerteam.chucker.api.ChuckerInterceptor");
+                Constructor constructor = clazz.getConstructor(Context.class);
+                Object obj = constructor.newInstance(LoginActivity.get().getApplicationContext());
+                chuckerInterceptor = (Interceptor) obj;
+                builder.addInterceptor(chuckerInterceptor);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        }).authenticator(RefreshAuthenticator.getInstance(tokenManager))
-                .addInterceptor(new ConnectivityInterceptor(LoginActivity.get().getApplicationContext()))
-                .build();
+        }
 
-        Retrofit newRetrofit = retrofit.newBuilder().client(newClient).baseUrl(TokenManager.getInstance().getServerUrl()).build();
+        Retrofit newRetrofit = retrofit.newBuilder().client(builder.build()).baseUrl(TokenManager.getInstance().getServerUrl()).build();
         return newRetrofit.create(service);
     }
 }
