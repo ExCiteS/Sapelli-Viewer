c906a4ca18784d2a201652b78970a89bd6c5d81f
Gulbala Salamov <g.salamov@gmail.com>
2020-02-01 17:57:01 +0300

    contribution detail screen is implemented

--- app/src/main/java/uk/ac/excites/ucl/sapelliviewer/ui/DetailsFragment.java
+++ app/src/main/java/uk/ac/excites/ucl/sapelliviewer/ui/DetailsFragment.java
@@ -1,15 +1,17 @@
 package uk.ac.excites.ucl.sapelliviewer.ui;
 
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.io.File;
 import java.util.Objects;
@@ -18,7 +20,6 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.schedulers.Schedulers;
 import uk.ac.excites.ucl.sapelliviewer.R;
-import uk.ac.excites.ucl.sapelliviewer.activities.OfflineMapsActivity;
 import uk.ac.excites.ucl.sapelliviewer.datamodel.Document;
 import uk.ac.excites.ucl.sapelliviewer.db.AppDatabase;
 import uk.ac.excites.ucl.sapelliviewer.db.DatabaseClient;
@@ -27,10 +28,12 @@ import uk.ac.excites.ucl.sapelliviewer.utils.Logger;
 import uk.ac.excites.ucl.sapelliviewer.utils.MediaHelpers;
 
 
-public class DetailsFragment extends Fragment implements DocumentFragmentListener {
-    private static final String CONTRIBUTION_ID = "contributionID";
+public class DetailsFragment extends DialogFragment implements DocumentFragmentListener {
+    private static final String CONTRIBUTION_ID = "contributionId";
+    private static final String PROJECT_ID = "projectId";
 
     private int contributionId;
+    private int projectId;
     private ContributionAudioAdapter audioAdapter;
     private ContributionPhotoAdapter photoAdapter;
     private RecyclerView valueRecyclerView;
@@ -43,10 +46,11 @@ public class DetailsFragment extends Fragment implements DocumentFragmentListene
         // Required empty public constructor
     }
 
-    public static DetailsFragment newInstance(int contributionId) {
+    public static DetailsFragment newInstance(int contributionId, int projectId) {
         DetailsFragment fragment = new DetailsFragment();
         Bundle args = new Bundle();
         args.putInt(CONTRIBUTION_ID, contributionId);
+        args.putInt(PROJECT_ID, projectId);
         fragment.setArguments(args);
         return fragment;
     }
@@ -55,7 +59,8 @@ public class DetailsFragment extends Fragment implements DocumentFragmentListene
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            contributionId = getArguments().getInt(CONTRIBUTION_ID);
+            contributionId = 30334;//getArguments().getInt(CONTRIBUTION_ID);
+            projectId = getArguments().getInt(PROJECT_ID);
         }
     }
 
@@ -74,34 +79,45 @@ public class DetailsFragment extends Fragment implements DocumentFragmentListene
     public void onStart() {
         super.onStart();
         AppDatabase db = AppDatabase.getAppDatabase(getActivity());
-        dbClient = ((OfflineMapsActivity) Objects.requireNonNull(getActivity())).getDbClient();
-        CompositeDisposable disposables = ((OfflineMapsActivity) Objects.requireNonNull(getActivity())).getDisposables();
+        dbClient = new DatabaseClient(getContext(), projectId, null);
+        CompositeDisposable disposables = new CompositeDisposable();
         valueRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
-        disposables.add(db.contributionDao().getPropertiesByContribution(contributionId).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
-                .subscribe(contributionProperties -> valueRecyclerView.setAdapter(new ContributionValueAdapter(getActivity(), contributionProperties))));
+        Log.d("contribution", "onStart: " + contributionId);
+        contributionId = 30340;
+        disposables.add(db.contributionDao().getPropertiesByContribution(contributionId)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(contributionProperties ->
+                {
+//                    Toast.makeText(getActivity(), "properties: " + contributionProperties.size(), Toast.LENGTH_SHORT).show();
+
+                    valueRecyclerView.setAdapter(new ContributionValueAdapter(getActivity(), contributionProperties));
+                }));
+
         photoRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
-        disposables.add(db.contributionDao().getPhotosByContribution(contributionId).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+
+        disposables.add(db.contributionDao().getPhotosByContribution(contributionId)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(photos -> {
-                    photoAdapter = new ContributionPhotoAdapter(getActivity(), photos, new ContributionPhotoAdapter.PhotoAdapterClickListener() {
-                        @Override
-                        public void onClick(Document photo) {
-                            openPhotoView(photo);
-                        }
-                    });
+//                    Toast.makeText(getActivity(), "photos: " + photos.size(), Toast.LENGTH_SHORT).show();
+                    photoAdapter = new ContributionPhotoAdapter(getActivity(), photos, photo -> openPhotoView(photo));
                     photoRecyclerView.setAdapter(photoAdapter);
                 }));
         audioRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
-        disposables.add(db.contributionDao().getAudiosByContribution(contributionId).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+
+        disposables.add(db.contributionDao().getAudiosByContribution(contributionId)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(audios -> {
-                    audioAdapter = new ContributionAudioAdapter(getActivity(), audios, new ContributionAudioAdapter.AudioAdapterClickListener() {
-                        @Override
-                        public void onClick(Document audio) {
-                            openAudioView(audio);
-                        }
-                    });
+//                    Toast.makeText(getActivity(), "audios: " + audios.size(), Toast.LENGTH_SHORT).show();
+                    audioAdapter = new ContributionAudioAdapter(getActivity(), audios, audio -> openAudioView(audio));
                     audioRecyclerView.setAdapter(audioAdapter);
                 }));
-        disposables.add(db.contributionDao().getDateByContribution(contributionId).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+
+        disposables.add(db.contributionDao().getDateByContribution(contributionId)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
                 .map(contributionProperty -> DateTimeHelpers.parseIso8601DateTime(contributionProperty.getValue())).subscribe(date -> dateTextView.setText(DateTimeHelpers.dateToString(date))));
 
     }
@@ -111,12 +127,12 @@ public class DetailsFragment extends Fragment implements DocumentFragmentListene
         FragmentManager fragmentManager = Objects.requireNonNull(getActivity()).getSupportFragmentManager();
         if (photo.isActive()) {
             fragmentManager.beginTransaction().remove(fragmentManager.findFragmentByTag(String.valueOf(photo.getId()))).commit();
-            dbClient.insertLog(Logger.PHOTO_CLOSED , photo.getId());
+            dbClient.insertLog(Logger.PHOTO_CLOSED, photo.getId());
         } else {
             PhotoFragment photoFragment = PhotoFragment.newInstance(photo.getId(), MediaHelpers.dataPath + File.separator + photo.getUrl());
 //            fragmentManager.beginTransaction().replace(R.id.fragment_media_container, photoFragment, String.valueOf(photo.getId())).commit();
             photoFragment.setFragmentListener(this);
-            dbClient.insertLog(Logger.PHOTO_OPENED , photo.getId());
+            dbClient.insertLog(Logger.PHOTO_OPENED, photo.getId());
         }
     }
 
@@ -124,12 +140,12 @@ public class DetailsFragment extends Fragment implements DocumentFragmentListene
         FragmentManager fragmentManager = Objects.requireNonNull(getActivity()).getSupportFragmentManager();
         if (audio.isActive()) {
             fragmentManager.beginTransaction().remove(fragmentManager.findFragmentByTag(String.valueOf(audio.getId()))).commit();
-            dbClient.insertLog(Logger.AUDIO_CLOSED , audio.getId());
+            dbClient.insertLog(Logger.AUDIO_CLOSED, audio.getId());
         } else {
             AudioFragment audioFragment = AudioFragment.newInstance(audio.getId(), MediaHelpers.dataPath + File.separator + audio.getUrl());
 //            fragmentManager.beginTransaction().replace(R.id.fragment_media_container, audioFragment, String.valueOf(audio.getId())).commit();
             audioFragment.setFragmentListener(this);
-            dbClient.insertLog(Logger.AUDIO_OPENED , audio.getId());
+            dbClient.insertLog(Logger.AUDIO_OPENED, audio.getId());
         }
     }
 
