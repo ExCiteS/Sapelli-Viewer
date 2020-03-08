6a06bb9ad97aa14ce68cee7e264ff06a171f97cb
Gulbala Salamov <g.salamov@gmail.com>
2020-03-08 22:33:47 +0300

    photo fragment is made dialog fragment and connected to DetailFragment

--- app/src/main/java/uk/ac/excites/ucl/sapelliviewer/ui/PhotoFragment.java
+++ app/src/main/java/uk/ac/excites/ucl/sapelliviewer/ui/PhotoFragment.java
@@ -1,24 +1,23 @@
 package uk.ac.excites.ucl.sapelliviewer.ui;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
-
-import android.view.LayoutInflater;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Objects;
+import com.bumptech.glide.Glide;
 
 import uk.ac.excites.ucl.sapelliviewer.R;
 
 
-public class PhotoFragment extends Fragment {
+public class PhotoFragment extends ProjectManagerFragment {
     private static final String PHOTO_ID = "photoId";
     private static final String PHOTO_PATH = "photoPath";
 
@@ -26,22 +25,38 @@ public class PhotoFragment extends Fragment {
     private String photoPath;
     private DocumentFragmentListener listener;
 
-
     public PhotoFragment() {
         // Required empty public constructor
     }
 
-    public void setFragmentListener(DocumentFragmentListener listener) {
-        this.listener = listener;
+    static PhotoFragment ShowDialog(AppCompatActivity owner, int photoId, String photoPath) {
+        PhotoFragment fragment = PhotoFragment.newInstance(photoId, photoPath);
+        fragment.show(owner.getSupportFragmentManager(), PhotoFragment.class.getSimpleName());
+        return fragment;
     }
 
-    public static PhotoFragment newInstance(int photoId, String photoPath) {
+    private static PhotoFragment newInstance(int photoId, String photoPath) {
         PhotoFragment fragment = new PhotoFragment();
         Bundle args = new Bundle();
         args.putInt(PHOTO_ID, photoId);
         args.putString(PHOTO_PATH, photoPath);
         fragment.setArguments(args);
         return fragment;
+    }
+
+    void setFragmentListener(DocumentFragmentListener listener) {
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getOwner());
+        AlertDialog dialog = builder.create();
+
+        // Set view:
+        setDialogView(dialog);
+        return dialog;
     }
 
     @Override
@@ -56,16 +71,33 @@ public class PhotoFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_photo, container, false);
-        ImageView photoView = view.findViewById(R.id.photo_view);
+    public void onStart() {
+        super.onStart();
+        Dialog d = getDialog();
+        if (d != null) {
+            int width = ViewGroup.LayoutParams.MATCH_PARENT;
+            int height = ViewGroup.LayoutParams.WRAP_CONTENT;
+            d.getWindow().setLayout(width, height);
+        }
+    }
 
-        Glide.with(Objects.requireNonNull(getActivity()))
+    @Override
+    protected void setupUI(AppCompatActivity owner, View rootLayout) throws Exception {
+        super.setupUI(owner, rootLayout);
+        ImageView photoView = rootLayout.findViewById(R.id.photo_view);
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(metrics.widthPixels, metrics.heightPixels);
+        photoView.setLayoutParams(params);
+
+        Glide.with(photoView.getContext())
                 .asBitmap()
                 .load(photoPath)
                 .into(photoView);
+    }
 
-        return view;
+    @Override
+    protected Integer getLayoutID() {
+        return R.layout.fragment_photo;
     }
 
     @Override
@@ -74,6 +106,4 @@ public class PhotoFragment extends Fragment {
         if (listener != null)
             listener.OnFragmentDetached(getClass().getSimpleName(), photoId);
     }
-
-
 }
